6e4c090265a6cbfce3ab08925dbdc49d4ac2fe60
Manish Kumar Prasad <manishraz32@gmail.com>
2025-02-04 15:19:39 +0530

    changed in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects</h2>
+      <h2>Hello Manish</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
